89ead418e61d98d5b902d605478684d5e7049ab0
bogdaninnova <bogdan.innova@gmail.com>
2015-11-26 02:06:47 +0200

    update

--- src/main/Calculator.java
+++ src/main/Calculator.java
@@ -6,25 +6,25 @@ public class Calculator {
 	private double dt = 0.001;
 
 	// Anisotropy axe's angles
-	public static final double at = 0.75 * Math.PI;
+	public static final double at = 0.0 * Math.PI;
 	public static final double af = 0 * Math.PI;
 
 	// m_theta, m_fi
-	public static double teta = at;
+	public static double teta = 0.1 * Math.PI;
 	public static double fi = af;
 
 	private final double alpha = 0.01;
 
 	//Circular field's characteristics
 	public static double h = 0.1;
-	public static double w = 0.75;
+	public static double w = 1;
 
 	//Runge-Kutta method
 	public void iteration() {
-		double[] k1 = LLG(teta, fi, t);
-		double[] k2 = LLG(teta + k1[0] * dt/2, fi + k1[1] * dt/2, t + dt/2);
-		double[] k3 = LLG(teta + k2[0] * dt/2, fi + k2[1] * dt/2, t + dt/2);
-		double[] k4 = LLG(teta + k3[0] * dt, fi + k3[1] * dt, t + dt);	
+		double[] k1 = LLG_w(teta, fi, t);
+		double[] k2 = LLG_w(teta + k1[0] * dt/2, fi + k1[1] * dt/2, t + dt/2);
+		double[] k3 = LLG_w(teta + k2[0] * dt/2, fi + k2[1] * dt/2, t + dt/2);
+		double[] k4 = LLG_w(teta + k3[0] * dt, fi + k3[1] * dt, t + dt);	
 		
 		teta += dt/6 * (k1[0] + 2 * k2[0] + 2 * k3[0] + k4[0]);
 		fi += dt/6 * (k1[1] + 2 * k2[1] + 2 * k3[1] + k4[1]);
@@ -66,7 +66,7 @@ public class Calculator {
 			(hy * cos(mf) - hx * sin(mf)) + mp * sin(at) * sin(af - mf) +
 			alpha * cos(mt) * ((hx * cos(mf) + hy * sin(mf)) + mp * sin(at) * cos(af - mf)) -
 			alpha * sin(mt) * (hz + mp * cos(at));
-
+		
 		//m_fi
 		double phi =
 				- cos(mt) / sin(mt) *
@@ -74,9 +74,40 @@ public class Calculator {
 					mp * sin(at) * cos(af - mf)) + hz + mp * cos(at) +
 				alpha / sin(mt) * (hy * cos(mf) - hx * sin(mf)) +
 				alpha / sin(mt) * mp * sin(at) * sin(af - mf);
+		phi /= alpha * alpha + 1;
+		thetta /= alpha * alpha + 1;
 		double[] array = {thetta, phi};
 		return array;
 	}
+	
+	
+	private double dW_dtheta(double mt, double mf, double t) {
+		return -( h * cos(mt) * cos(mf) * cos(w * t) + 
+				h * cos(mt) * sin(mf) * sin(w * t) +
+				m_p(mt, mf) * (sin(at) * cos(mt) * cos(af - mf) - cos(at) * sin(mt)));
+	}
+	
+	private double dW_dphi(double mt, double mf, double t) {
+		return -( h * sin(mt) * sin(mf) * cos(w * t) + 
+				h * sin(mt) * cos(mf) * sin(w * t) +
+				m_p(mt, mf) * (sin(at) * sin(mt) * sin(af - mf)));
+	}
+	
+	
+	private double[] LLG_w(double mt, double mf, double t) {
+		double thetta = - 1 / sin(mt) *
+				(alpha * sin(mt) * dW_dtheta(mt, mf, t) + dW_dphi(mt, mf, t));
+		double phi = - 1 / sin(mt) / sin(mt) *
+				(sin(mt) * dW_dtheta(mt, mf, t) - alpha * dW_dphi(mt, mf, t));
+		phi /= alpha * alpha + 1;
+		thetta /= alpha * alpha + 1;
+		double[] array = {thetta, phi};
+		return array;
+	}
+	
+	
+	
+	
 	
 	private double sin(double a) {
 		return Math.sin(a);
