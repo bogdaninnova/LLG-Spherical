57c761ff8ea2fc4976105b3738be6a51f22216fc
User <User@User-PC>
2016-01-08 13:20:58 +0200

    Some edition

--- src/main/Calculator.java
+++ src/main/Calculator.java
@@ -3,10 +3,10 @@ package main;
 public class Calculator {
 
 	private double t = 0;
-	private double dt = 0.001;
+	private double dt = 0.01;
 
 	// Anisotropy axe's angles
-	public static final double at = 0.0 * Math.PI;
+	public static final double at = 0 * Math.PI;
 	public static final double af = 0 * Math.PI;
 
 	// m_theta, m_fi
@@ -16,36 +16,55 @@ public class Calculator {
 	private final double alpha = 0.01;
 
 	//Circular field's characteristics
-	public static double h = 0.1;
+	public static double h = 0.2;
 	public static double w = 1;
+	
+	
+	public double dm_theta = 0;
+	public double dm_fi = 0;
+
+	public double hx = 0;
+	public double hy = 0;
+	public double hz = 0;
+	
+	
 
 	//Runge-Kutta method
 	public void iteration() {
-		double[] k1 = LLG_w(teta, fi, t);
-		double[] k2 = LLG_w(teta + k1[0] * dt/2, fi + k1[1] * dt/2, t + dt/2);
-		double[] k3 = LLG_w(teta + k2[0] * dt/2, fi + k2[1] * dt/2, t + dt/2);
-		double[] k4 = LLG_w(teta + k3[0] * dt, fi + k3[1] * dt, t + dt);	
+		double[] k1 = LLG(teta, fi, t);
+		double[] k2 = LLG(teta + k1[0] * dt/2, fi + k1[1] * dt/2, t + dt/2);
+		double[] k3 = LLG(teta + k2[0] * dt/2, fi + k2[1] * dt/2, t + dt/2);
+		double[] k4 = LLG(teta + k3[0] * dt, fi + k3[1] * dt, t + dt);	
 		
-		teta += dt/6 * (k1[0] + 2 * k2[0] + 2 * k3[0] + k4[0]);
-		fi += dt/6 * (k1[1] + 2 * k2[1] + 2 * k3[1] + k4[1]);
+		dm_theta = dt/6 * (k1[0] + 2 * k2[0] + 2 * k3[0] + k4[0]);
+		dm_fi = dt/6 * (k1[1] + 2 * k2[1] + 2 * k3[1] + k4[1]);
+		
+		teta +=dm_theta;
+		fi += dm_fi;
 		t += dt;
+	}
+	
+	public double getdQ() {
+		return hx * sin(dm_theta) * cos(dm_fi) +
+				hy * sin(dm_theta) * sin(dm_fi) +
+				hz * cos(dm_theta);
 	}
 	
 	private double hx(double mt, double mf, double t) {
 		double ha_x = m_p(mt, mf) * sin(at) * cos(af);
 		double ho_x = h * Math.cos(w * t);
-		return ha_x + ho_x;
+		return hx = ha_x + ho_x;
 	}
 	
 	private double hy(double mt, double mf, double t) {
 		double ha_y = m_p(mt, mf) * sin(at) * sin(af);
 		double ho_y = h * Math.sin(w * t);
-		return ha_y + ho_y;
+		return hy = ha_y + ho_y;
 	}
 	
 	private double hz(double mt, double mf, double t) {
 		double ha_z = m_p(mt, mf) * cos(at);
-		return ha_z;
+		return hz = ha_z;
 	}
 
 	private double m_p(double mt, double mf) {
@@ -109,11 +128,11 @@ public class Calculator {
 	
 	
 	
-	private double sin(double a) {
+	private static double sin(double a) {
 		return Math.sin(a);
 	}
 	
-	private double cos(double a) {
+	private static double cos(double a) {
 		return Math.cos(a);
 	}
 	
--- src/main/Launcher.java
+++ src/main/Launcher.java
@@ -7,29 +7,45 @@ import java.util.*;
 public class Launcher {
 
 	public static void main(String...strings) {
+		for (double w = 0.1; w <=2.001; w += 0.1)
+			System.out.println(energy(w));
+	}
+	
+	public static double energy(double w) {
 		Calculator c = new Calculator();
-		int k = 500000;
+		c.w = w;
+		int k = 5000;
 		ArrayList<Double> listX = new ArrayList<Double>(k);
 		ArrayList<Double> listY = new ArrayList<Double>(k);
 		ArrayList<Double> listZ = new ArrayList<Double>(k);
 		
-		for (int i = 0; i < 1000000; i++)
+		for (int i = 0; i < 50000; i++)
 			c.iteration();
 
 		LinkedList<Vector> list = new LinkedList<Vector>();
+		
+		double energy = 0;
+		double steps = 0;
+		
 		while (k --> 0) {
 			c.iteration();
+			energy += c.getdQ();
+			steps++;
 			listX.add(Math.sin(c.teta) * Math.cos(c.fi));
 			listY.add(Math.sin(c.teta) * Math.sin(c.fi));
 			listZ.add(Math.cos(c.teta));
 			list.add(new Vector(c.teta, c.fi));
 		}
 		
-		writeDoubleList(listX, "listX");
-		writeDoubleList(listY, "listY");
-		writeDoubleList(listZ, "listZ");
+		
+		
+//		writeDoubleList(listX, "listX");
+//		writeDoubleList(listY, "listY");
+//		writeDoubleList(listZ, "listZ");
 		
 		new Draw(list, 0.4 * Math.PI, 0.4 * Math.PI, 0, "w=" + Calculator.w + ";h=" + Calculator.h + ";teta=" + Calculator.at).drawTraectory(true);
+
+		return(energy / steps);
 	}
 	
 	public static void writeDoubleList(List<Double> averrageList, String name) {
